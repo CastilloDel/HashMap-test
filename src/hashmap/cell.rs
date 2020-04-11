98be0b8178f661d87006230722d52954e8576208
CastilloDel <delcastillodelarosadaniel@gmail.com>
2020-04-11 16:45:51 +0100

    Create the cell of the hashmap

--- src/hashmap/cell.rs
+++ src/hashmap/cell.rs
@@ -1,20 +1,43 @@
-pub struct Cell<T> 
-  where T: PartialEq {
-  keys: Vec<T>,
+use std::{error::Error, fmt};
+
+#[derive(Debug)]
+pub struct CellFull;
+
+impl Error for CellFull {}
+
+impl fmt::Display for CellFull {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Tried to insert an item in a cell which was full")
+    }
 }
 
-impl<T: PartialEq> Cell<T> {
-  fn new(size: usize) -> Cell<T> {
-    Cell{
-    keys: Vec::with_capacity(size)
+pub struct Cell<T>
+where
+    T: PartialEq + Clone,
+{
+    keys: Vec<T>,
+}
+
+impl<T: PartialEq + Clone> Cell<T> {
+    pub fn new(size: usize) -> Cell<T> {
+        Cell {
+            keys: Vec::with_capacity(size),
+        }
     }
-  }
 
-  fn full(&self) -> bool {
-    return self.keys.len() == self.keys.capacity()
-  }
+    pub fn full(&self) -> bool {
+        self.keys.len() == self.keys.capacity()
+    }
 
-  fn contains(&self, key: &T) -> bool {
-    self.keys.iter().any(|a| a == key)
-  }
+    pub fn contains(&self, key: &T) -> bool {
+        self.keys.iter().any(|a| a == key)
+    }
+
+    pub fn insert(&mut self, key: &T) -> Result<(), CellFull> {
+        if self.full() {
+            return Err(CellFull {});
+        }
+        self.keys.push(key.clone());
+        Ok(())
+    }
 }
